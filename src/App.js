8a98c7613c4b058bf40eb068b33470136cb5b58a
tysiote <martinusmaco@gmail.com>
2019-01-03 09:05:33 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Calendar from './components/Calendar';
+import Portfolio from './components/Portfolio';
+import Container from "reactstrap/es/Container";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calendar_url : 'https://www.googleapis.com/calendar/v3/calendars/martinusmaco@gmail.com/events?key=AIzaSyCEkYFfS9avBnjXP1xX5EJBm2gm3rrHuh4'
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Container>
+            <Calendar url={this.state.calendar_url}/>
+            <Portfolio/>
+        </Container>
     );
   }
 }
